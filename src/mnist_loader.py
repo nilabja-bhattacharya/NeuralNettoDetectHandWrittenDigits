9e7f506aa812e018db90c3e36ba447c6799baba4
nilabja10201992 <nilabja10201992@gmail.com>
2017-04-06 20:40:47 +0530

    update

--- src/mnist_loader.py
+++ src/mnist_loader.py
@@ -41,10 +41,10 @@ def load_data_wrapper():
     code."""
     tr_d,va_d,te_d = load_data()
     #print (tr_d)
-    training_inputs = [np.reshape(x,(784,1)).astype('float32')/255 for x in tr_d[0]]
+    training_inputs = [np.reshape(x,(784,1)) for x in tr_d[0]]
     training_results = [vectorized_results(y) for y in tr_d[1]]
     training_data = zip(training_inputs,training_results)
-    validation_inputs = [np.reshape(x,(784,1)).astype('float32')/255 for x in va_d[0]]
+    validation_inputs = [np.reshape(x,(784,1)) for x in va_d[0]]
     validation_data = zip(validation_inputs,va_d[1])
     test_inputs = [np.reshape(x,(784,1)) for x in te_d[0]]
     test_data = zip(test_inputs,te_d[1])
